63f88ba8f1a07fef0cd3e03ba3c4d665d978f77a
Ronan McCabe <ronan_mccabe@hotmail.com>
2015-07-09 01:36:07 +0100

    analytics tinkering

--- js/handlers.js
+++ js/handlers.js
@@ -6,13 +6,19 @@ function addNewUser (id) {
     visits: 1,
     time: new Date().getTime()
   };
-  redis.startRedisClient(analytics);
+  // redis.startRedisClient(analytics);
 }
 
 module.exports = {
 
   home: function(request, reply) {
     // request.log(); // triggers the good-http log on the server to the /analytics endpoint
+    console.log(request.id);
+    console.log(request.info.remoteAddress);
+    console.log(request.info.host);
+    console.log(request.info.hostname);
+    console.log(request.connection.info);
+    console.log(request.state.userId);
     if(request.headers.cookie) {
       addNewUser(request.headers.cookie.split('=')[1]);
     }
