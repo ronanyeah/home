ce7df789d325121e0c726699edd70b8b760b1dfe
ronanyeah <ronan_mccabe@hotmail.com>
2016-11-20 02:55:37 +0000

    Add push notification server tool.

---
+++ tools/pushNotify.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const fs = require('fs')
+const R  = require('ramda')
+const wp = require('web-push')
+
+const subscriptionsPath = `${global.ROOT}/private/push_subscriptions.json`
+const vapidKeysPath     = `${global.ROOT}/private/vapid_keys.json`
+
+fs.existsSync(subscriptionsPath)
+  ? null
+  : fs.writeFileSync(
+      subscriptionsPath,
+      '[]'
+    )
+
+fs.existsSync(vapidKeysPath)
+  ? null
+  : fs.writeFileSync(
+      vapidKeysPath,
+      JSON.stringify( wp.generateVAPIDKeys() )
+    )
+
+const vapidKeys = require(vapidKeysPath)
+
+const removeSubscription = endpoint =>
+  fs.writeFileSync(
+    subscriptionsPath,
+    JSON.stringify(
+      require(subscriptionsPath)
+      .filter(
+        subscription =>
+          subscription.endpoint !== endpoint
+      )
+    )
+  )
+
+const addSubscription = newSub => {
+  const subscriptions = require(subscriptionsPath)
+
+  return subscriptions.find(
+    subscription =>
+      subscription.endpoint === newSub.endpoint
+  )
+    ? null
+    : fs.writeFileSync(
+        subscriptionsPath,
+        JSON.stringify( R.append(newSub, subscriptions) )
+      )
+}
+
+const push = (title = 'HEY', body = '') =>
+  require(subscriptionsPath)
+  .forEach(
+    subscription =>
+      wp.sendNotification(
+        subscription,
+        JSON.stringify({ title, body }),
+        {
+          vapidDetails: {
+            subject: 'mailto:hey@ronanmccabe.me',
+            publicKey: vapidKeys.publicKey,
+            privateKey: vapidKeys.privateKey
+          }
+        }
+      )
+      .catch(
+        err => {
+          console.log(err.message)
+          return err.statusCode === 410 // 410 means invalid subscription.
+            ? removeSubscription(subscription.endpoint)
+            : null
+        }
+      )
+  )
+
+module.exports = {
+  addSubscription,
+  removeSubscription,
+  push
+}
