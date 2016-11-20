98cfb086e13b42dd1ddc01cca129c8eac0e185f9
ronanyeah <ronan_mccabe@hotmail.com>
2016-11-20 02:57:36 +0000

    Refactor ipCheck script, send push notification.

--- scripts/ipCheck.js
+++ scripts/ipCheck.js
@@ -4,41 +4,44 @@ const co       = require('co')
 const bluebird = require('bluebird')
 const fs       = bluebird.promisifyAll( require('fs') )
 
-const { senderEmail, password } = require(`${global.ROOT}/private/mailConfig.json`)
+// CONFIG
+const ipLogPath = `${global.ROOT}/ipLog.json`
+const {
+  senderEmail,
+  password
+} = require(`${global.ROOT}/private/mailConfig.json`)
+const {
+  zoneId, authKey,
+  emailAddress, dnsRecordIds
+} = require(`${global.ROOT}/private/cloudflareConfig.json`)
+// zoneId: String,
+// - id of the cloudflare website instance
 
-const sendEmail =
-  require(`${global.ROOT}/tools/emailer.js`)(senderEmail, password)
+// authKey: String,
+// - api key
 
-const cfConfig = require(`${global.ROOT}/private/cloudflareConfig.json`)
-// {
-  // zoneId: String,
-  // - id of the cloudflare website instance
+// emailAddress: String,
+// - cloudflare email
 
-  // authKey: String,
-  // - api key
+// dnsRecordIds: Array<String>
+// - the DNS records that need to have their ip addresses updated.
+// your DNS records can be found at:
+// https://api.cloudflare.com/client/v4 +
+// /zones/<YOUR-ZONE-ID>/dns_records
 
-  // emailAddress: String,
-  // - cloudflare email
-
-  // dnsRecordIds: Array<String>
-  // - the DNS records that need to have their ip addresses updated.
-  // your DNS records can be found at:
-  // https://api.cloudflare.com/client/v4 +
-  // /zones/<YOUR-ZONE-ID>/dns_records
-// }
-
+// SUPPORT FUNCTIONS
+const sendEmail = require(`${global.ROOT}/tools/emailer.js`)(senderEmail, password)
+const { push } = require(`${global.ROOT}/tools/pushNotify.js`)
 const { updateDnsRecordIp, getCurrentIp } = require(`${global.ROOT}/tools/cloudflare.js`)
 
 module.exports = _ =>
   co(function* () {
 
-    const ipLogPath = `${global.ROOT}/ipLog.json`
-
     const currentIp = yield getCurrentIp()
 
-    yield cfConfig.dnsRecordIds.map(
+    yield dnsRecordIds.map(
       id => updateDnsRecordIp(
-        currentIp, id, cfConfig.zoneId, cfConfig.emailAddress, cfConfig.authKey
+        currentIp, id, zoneId, emailAddress, authKey
       )
     )
 
@@ -51,9 +54,10 @@ module.exports = _ =>
 
     return currentIp === oldIp
       ? 'no change'
-      : yield [
+      : (
+          push('NEW IP', currentIp),
           sendEmail(
-            cfConfig.emailAddress,
+            emailAddress,
             'Rasperry Pi',
             'IP Update',
             `${ currentIp } - ${ Date() }`
@@ -62,7 +66,6 @@ module.exports = _ =>
             ipLogPath,
             JSON.stringify({ ip: currentIp })
           )
-        ]
+        )
   })
-  .then( console.log )
-  .catch( console.log )
+  .catch( err => push('ipCheck Error', err.message) )
