509725c9371617e69d6e183860a86fb29cebee67
ronanyeah <ronan_mccabe@hotmail.com>
2016-11-22 00:58:42 +0000

    Refactor server and router. Again.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const http  = require('http')
+const https = require('https')
 const fs    = require('fs')
 const path  = require('path')
 
 global.ROOT = path.resolve(`${__dirname}/../`)
 
+// Check that support files exist.
 require(`${global.ROOT}/tools/checkForSupportFiles`)(
   [
     `${global.ROOT}/private/mailConfig.json`,
@@ -14,32 +17,36 @@ require(`${global.ROOT}/tools/checkForSupportFiles`)(
   ]
 )
 
-const { http, https } = require(`${global.ROOT}/server/server.js`)
-
+const router = require(`${global.ROOT}/server/router.js`)
 const server =
-  process.env.NODE_ENV === 'development'
-    ? http
-    : https(
-        fs.readFileSync(`${global.ROOT}/private/pvt.pem`),
-        fs.readFileSync(`${global.ROOT}/private/cert.pem`)
-      )
+    process.env.NODE_ENV === 'development'
+      ? http.createServer()
+      : https.createServer(
+          {
+            key: fs.readFileSync(`${global.ROOT}/private/pvt.pem`),
+            cert: fs.readFileSync(`${global.ROOT}/private/cert.pem`)
+          }
+        )
 
-const port = process.env.PORT || 443
-
-server.listen(
-  port,
-  _ => {
+server
+.listen(
+  process.env.PORT || 443
+)
+.on(
+  'listening',
+  _ =>
     console.log(
-      `${
+      `\n${
          process.env.NODE_ENV === 'development' ? 'http' : 'https'
-       } server listening on port ${port}`
+       } server listening on port ${ server.address().port }`
     )
+)
+.on( 'request', router )
 
-    return fs.readdirSync(`${global.ROOT}/scripts/`)
-    .forEach(
-      file =>
-        // Run every five minutes.
-        setInterval(require(`${global.ROOT}/scripts/${file}`), 300000)
-    )
-  }
+// START UTILITY SCRIPTS
+fs.readdirSync(`${global.ROOT}/scripts/`)
+.forEach(
+  file =>
+    // Run every five minutes.
+    setInterval(require(`${global.ROOT}/scripts/${file}`), 300000)
 )
--- server/router.js
+++ server/router.js
@@ -1,10 +1,23 @@
 'use strict'
 
 const R = require('ramda')
+const url   = require('url')
+const publicFolder = `${global.ROOT}/client/public`
 
 const handlers = require(`${global.ROOT}/server/handlers/index.js`)
+const assets = require(`${global.ROOT}/tools/assetIndexer.js`)(publicFolder)
+const { sendFile } = require(`${global.ROOT}/server/helpers.js`)
+const { error } = require(`${global.ROOT}/server/handlers/index.js`)
 
-// The Router:
 module.exports =
-  (method, path) =>
-    R.path([method, path], handlers) || handlers['GET']['/fourOhFour']
+  (req, res) =>
+    req.method === 'GET' && assets.includes( url.parse(req.url).pathname )
+      ? sendFile(publicFolder + req.url)(req, res)
+        .catch( err => error(err)(req, res) )
+      : (
+          R.path(
+            [ req.method, url.parse(req.url).pathname ],
+            handlers
+          ) || handlers['GET']['/fourOhFour']
+        )(req, res)
+        .catch( err => error(err)(req, res) )
