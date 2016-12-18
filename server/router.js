c31093176f927d9e8b4b8ecf1d3bf5c7598e6ca8
ronanyeah <ronan_mccabe@hotmail.com>
2016-12-18 12:30:18 +0000

    Simplify asset path caching.

--- server/router.js
+++ server/router.js
@@ -1,22 +1,19 @@
 'use strict'
 
-const { path, map, drop } = require('ramda')
-const { fileMapper }      = require('rotools')
+const { path } = require('ramda')
+const { fileMapper } = require('rotools')
 
 const handlers = require(`${ROOT}/server/handlers/index.js`)
 const { sendFile } = require(`${ROOT}/server/helpers.js`)
 
 const publicFolder = `${ROOT}/client/public`
-const assetPaths =
-  map(
-    drop(publicFolder.length),
-    fileMapper(publicFolder)
-  )
+
+const assetPaths = fileMapper(publicFolder)
 
 // String -> String -> Future Err Function
 module.exports =
   (method, pathname) =>
-    method === 'GET' && assetPaths.includes( pathname )
+    method === 'GET' && assetPaths.includes( publicFolder + pathname )
       ? () => sendFile( publicFolder + pathname )
       : path(
           [ method, pathname ],
