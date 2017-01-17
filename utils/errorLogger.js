e31e9bff5b073ed53f34deb0b1aebe1819ab655a
ronanyeah <ronan_mccabe@hotmail.com>
2017-01-17 03:23:27 +0000

    Improve error logging.

--- utils/errorLogger.js
+++ utils/errorLogger.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const logfile = `${ROOT}/errorLog.txt`
+const { homedir } = require('os')
 const { appendFile } = require('fs')
+
+const logfile = `${homedir()}/storage/code/home_errorLog.txt`
 
 module.exports = err => (
   console.log(err),
-  appendFile(logfile, `${err.message}\n\n`)
+  appendFile(logfile, `${Date()}\n${err.message}\n\n`)
 )
