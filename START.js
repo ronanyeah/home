b7648fd32d9b53e3af575fa3c7cebde1234bc10c
ronanyeah <hey@ronanmccabe.me>
2017-01-18 00:04:25 +0000

    fix CWD in START script

--- START.js
+++ START.js
@@ -4,7 +4,7 @@ const { exec } = require('rotools')
 
 const errorLogger = require(`${__dirname}/utils/errorLogger.js`)
 
-exec('HTTPS=true IP_SYNC=true pm2 start ./server/index.js')
+exec(`HTTPS=true IP_SYNC=true pm2 start ${__dirname}/server/index.js`)
 .fork(
   errorLogger,
   console.log
