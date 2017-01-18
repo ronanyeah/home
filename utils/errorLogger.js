dae004fd60dd2130a9a43ef16f6d5adbf2e6ad6e
ronanyeah <ronan_mccabe@hotmail.com>
2017-01-18 00:40:30 +0000

    Simplify error logger.

--- utils/errorLogger.js
+++ utils/errorLogger.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const { homedir } = require('os')
 const { appendFile } = require('fs')
-
-const logfile = `${homedir()}/storage/code/home_errorLog.txt`
 
 module.exports = err => (
   console.log(err),
-  appendFile(logfile, `${Date()}\n${err.message}\n\n`)
+  appendFile(
+    `${__dirname}/error_log.txt`,
+    `${Date()}\n${err.message}\n\n`
+  )
 )
