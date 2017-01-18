f8ab2cf7075074aa626a0abe3b1b0417709ad9b1
ronanyeah <hey@ronanmccabe.me>
2017-01-18 00:46:33 +0000

    use local pm2

--- START.js
+++ START.js
@@ -4,7 +4,7 @@ const { exec } = require('rotools')
 
 const errorLogger = require(`${__dirname}/utils/errorLogger.js`)
 
-exec(`HTTPS=true IP_SYNC=true pm2 start ${__dirname}/server/index.js`)
+exec(`HTTPS=true IP_SYNC=true ${__dirname}/node_modules/.bin/pm2 start ${__dirname}/server/index.js`)
 .fork(
   errorLogger,
   console.log
