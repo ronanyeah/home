177d4c977dcd4f830d5cdfd2fefc43064d761e6c
ronan <ronan_mccabe@protonmail.com>
2017-04-21 22:22:28 +0300

    remove logentries

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,3 @@
 'use strict'
 
-const Logentries = require('le_node')
-
-const logger =
-  process.env.NODE_ENV === 'production'
-    ? new Logentries({
-        token: process.env.LOGENTRIES_TOKEN
-      })
-    : { info: console.log }
-
 module.exports = console.log
